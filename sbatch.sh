1998e69113f4697169e622c7eccca9bd739e7b8c
shreyash2106 <shreyash2106@berkeley.edu>
2025-04-23 17:52:20 -0400

    latest experiments

--- sbatch.sh
+++ sbatch.sh
@@ -14,6 +14,6 @@ source stego_env/bin/activate
 export WANDB_API_KEY=38619006c8e9a24d38d1e4433dcd882a7259ee6b
 
 # python3 main.py --mode train --config configs/baseline_no_transforms.json --checkpoint best
-#python3 -u -m src.main train --config configs/rotation_robust_binary.yaml
-python3 -u -m src.main test_rotation --config configs/rotation_robust_binary.yaml
+#python3 -u -m src.main train --config configs/rotation_robust.yaml
+python3 -u -m src.main test_rotation --config configs/rotation_robust.yaml
 #python3 -u -m src.main experiment_rotation --config configs/rotation_robust.yaml
